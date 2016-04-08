c0f54993699e308ab308374b52bbfe083e45d6ab
Sy Morikawa <sy@hawaii.edu>
2016-04-07 14:27:08 -1000

    finished react todos

--- simple-todos/imports/ui/app.jsx
+++ simple-todos/imports/ui/app.jsx
@@ -48,9 +48,18 @@ class App extends Component {
     if (this.state.hideCompleted) {
       filteredTasks = filteredTasks.filter(task => !task.checked);
     }
-    return filteredTasks.map((task) => (
-      <Task key={task._id} task={task}/>
-    ));
+    return filteredTasks.map((task) => {
+      const currentUserId = this.props.currentUser && this.props.currentUser._id;
+      const showPrivateButton = task.owner === currentUserId;
+
+      return (
+        <Task
+          key={task._id}
+          task={task}
+          showPrivateButton={showPrivateButton}
+        />
+      );
+    });
   }
 
   render() {
@@ -94,9 +103,13 @@ App.propTypes = {
   tasks: PropTypes.array.isRequired,
   incompleteCount: PropTypes.number.isRequired,
   currentUser: PropTypes.object,
+  showPrivateButton: React.PropTypes.bool.isRequired,
 };
 
 export default createContainer(() => {
+
+  Meteor.subscribe('tasks');
+
   return {
     tasks: Tasks.find({}, {sort: {createdAt: -1}}).fetch(),
     incompleteCount: Tasks.find({checked: {$ne: true}}).count(),
